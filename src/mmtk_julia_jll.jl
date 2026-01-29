31516177e4dafdc5cd9785c71b9fe6eab9f8f646
jlbuild <juliabuildbot@gmail.com>
2026-01-29 00:01:54 +0000

    mmtk_julia_jll build 0.31.2+0

--- src/mmtk_julia_jll.jl
+++ src/mmtk_julia_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("mmtk_julia")
-JLLWrappers.@generate_main_file("mmtk_julia", UUID("9417aab8-0bea-54fa-920e-1be37e111af1"))
+JLLWrappers.@generate_main_file("mmtk_julia", Base.UUID("9417aab8-0bea-54fa-920e-1be37e111af1"))
 end  # module mmtk_julia_jll
